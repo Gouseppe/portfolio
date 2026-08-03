1cbce4be0d72b26a1141f1c441e680cf4829b7e4
Gouseppe <fernandeitorc@gmail.com>
2026-08-03 16:56:25 -0400

    fix: update redirectToDefaultLocale setting and refine index.astro entry point

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ export default defineConfig({
     locales: ["en", "es"],
     routing: {
       prefixDefaultLocale: true,
-      redirectToDefaultLocale: true,
+      redirectToDefaultLocale: false,
     },
   },
 });
